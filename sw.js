d8786969fe998cdcae43a21a29eb4b3226380895
Sukuna <1064687807@qq.com>
2024-12-25 17:23:48 +0800

    Site updated: 2024-12-25 17:23:26

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1735117253934";
+const VERSION = "1735118600001";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
